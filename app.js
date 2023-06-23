eb04621f23c740d97a02d372d0d70cb4adef0558
martin <martin.scarponi.ms@gmail.com>
2023-06-22 22:35:07 -0300

    TP 5 con correccion

--- app.js
+++ app.js
@@ -3,28 +3,27 @@ const app = express();
 const PORT = 3000;
 
 //Rutas
-app.get("/", (req,res) => {
-    res.send("Estoy en home")
-})
+app.get("/", (req, res) => {
+  res.send("Estoy en home");
+});
 
-app.get("/login", (req,res) => {
-    res.send("Estoy en login")
-})
+app.get("/login", (req, res) => {
+  res.send("Estoy en login");
+});
 
-app.get("/register", (req,res) => {
-    res.send("Estoy en register")
-})
+app.get("/register", (req, res) => {
+  res.send("Estoy en register");
+});
 
-app.get("/detailproduct", (req,res) => {
-    res.send("Estoy en detail product")
-})
+app.get("/detailproduct", (req, res) => {
+  res.send("Estoy en detail product");
+});
 
-app.get("/cart", (req,res) => {
-    res.send("Estoy en cart")
-})
+app.get("/cart", (req, res) => {
+  res.send("Estoy en cart");
+});
 
-
-//Server 
+//Server
 app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`);
-  });
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
